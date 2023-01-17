f2ddaba14fc0017b40186c33256a43faa9ee1327
davidsilva131 <davidsilvac131@gmail.com>
2023-01-17 13:05:59 -0500

    Fix header responsive

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -3,7 +3,7 @@ import Search from "./Search";
 
 const Header: FC<{}>=()=>{
   return(
-    <header className="mt-20 px-10 w-screen flex flex-col justify-center">
+    <header className="mt-20 px-10  w-full flex flex-col justify-center">
       <section className="m-auto flex flex-col gap-8 justify-center">
         <h1 className="text-5xl font-Crimson font-black text-center text-white">ELIGE TU CAMPEÓN</h1>
         <h2 className="w-full lg:w-7/12 m-auto text-lg font-Signika text-white text-center">Con más de 140 campeones, encontrarás el que se ajuste perfectamente a tu estilo de juego. Domina a uno o domínalos a todos.</h2>
