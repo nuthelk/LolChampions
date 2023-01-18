f06c78023b29e2fe39b84fa28cbc9cfd19f476b4
Nuthelk Molina <nuthelkcd@gmail.com>
2023-01-17 19:08:35 -0500

    info de champ

--- src/pages/DeatilChamp.tsx
+++ src/pages/DeatilChamp.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react';
 import { ChampionSingle } from '../interfaces/ChampionSingle';
 import { dataChamp } from '../services/lolAPI'
+import Info from '../components/Info';
 
 const DeatilChamp = () => {
     const [dataChampion, setDataChampion] = useState<ChampionSingle>()
@@ -13,23 +14,19 @@ const DeatilChamp = () => {
     useEffect(() => {
         dataChamp(setDataChampion, url)
     }, [])
-
+   
 
     return (
         <div
             style={{ backgroundImage: `url(${urlImage})` }}
             className={`bg-cover bg-no-repeat bg-center before:bg-[rgba(0,0,0,.7)] before:backdrop-saturate-50 overflow-auto before:backdrop-blur-sm before:inset-0 before:absolute relative min-h-screen w-full font-Signika flex flex-col items-center `}>
-            <div className='relative mt-14 rounded-lg px-4 text-center flex justify-center '>
-                <img src={urlImage} alt="" className='rounded-lg max-h-[600px]' />
-                
-                <p className='text-white italic md:text-xl absolute bottom-14 lg:bottom-20 xl:bottom-24 md:bottom-20 lg:text-3xl '>{dataChampion?.title}</p>
-
-                <div className='flex flex-col items-center bottom-4 md:bottom-6 lg:bottom-0 absolute'>
-                    <h1 className='relative text-white italic text-4xl md:text-5xl lg:text-7xl xl:text-8xl uppercase'>{dataChampion?.name}</h1>
-                    <div className='flex items-center flex-col justify-around'>
-                    </div>
-                </div>
-            </div>
+            <Info 
+                urlImage={urlImage}
+                title={dataChampion?.title}
+                name={dataChampion?.name}
+                tags={dataChampion?.tags}
+                lore={dataChampion?.lore}
+            />
 
         </div>
     )
