7ca24702ce1bdb4efac02ca70cae48ed96cc6f2e
davidsilva131 <davidsilvac131@gmail.com>
2023-01-19 10:54:52 -0500

    Add search functionality(select incomplete, bacground need to be fixed), add transition to champion types, fix search responsive, add skins designs and animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,22 +3,27 @@ import Card from '../components/Card'
 import { Champion } from '../interfaces'
 import {dataAPI} from '../services/lolAPI'
 import Header from '../components/home/Header';
+import Search from '../components/home/Search';
 
 const Home = () => {
 
     const [data, setData] = useState<Champion[]>([])
     
-
+    const [dataFiltered, setDataFiltered] = useState()
     useEffect(()=>{
         dataAPI(setData)
     },[])
+
+    useEffect(()=>{
+        setDataFiltered(data)
+    },[data])
     
   return (
     <div className='w-full h-full home py-4'>
-        <Header />
+        <Header champions={data} setChampions={setDataFiltered}/>
         <div className='flex flex-wrap gap-1 w-4/5 mx-auto '>
         {
-            data.map((champ, i) => (
+            dataFiltered?.map((champ, i) => (
                 <Card key={champ.id}
                 name={champ.name}
                 id={champ.id}
