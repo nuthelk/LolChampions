decac16292778151a39ad6417dc3be26a466ef03
davidsilva131 <davidsilvac131@gmail.com>
2023-01-19 08:36:41 -0500

    Update skins design

--- src/components/detailChamp/Skins.tsx
+++ src/components/detailChamp/Skins.tsx
@@ -21,19 +21,19 @@ const Skins = ({ skinsData, championId }) => {
   return (
     <>
       {/* <h2 className="relative mt-14 text-white font-Signika text-xl md:text-5xl lg:text-7xl xl:text-8xl uppercase">Aspectos</h2> */}
-      <section className="flex w-full flex-col items-center px-4 mb-10">
-        <aside className="relative mt-14 px-4 rounded-lg text-center flex flec-col justify-center text-center">
-          <img className="max-h-[600px] rounded-md " src={skinSelected.img} alt={skinSelected.name}/>
+      <section className="flex w-full flex-col items-center px-4 mb-10 animateLoad">
+        <aside className="relative mt-14 px-4 rounded-lg flex flec-col justify-center text-center">
+          <img className="max-h-[600px] rounded-md" src={skinSelected.img} alt={skinSelected.name}/>
           <div className="flex flex-col items-center bottom-4 md:bottom-6 lg:bottom-0 absolute" >
             <h3 className=" relative text-white font-Signika italic text-xl md:text-5xl lg:text-7xl xl:text-8xl uppercase">{skinSelected.name}</h3>
           </div>
         </aside>
-        <div className= "w-3/4 xl:w-3/6 flex relative gap-4 p-4 overflow-x-auto scroll-smooth scrollbar-thin scrollbar-thumb-secondary scrollbar-track-primary ">
+        <div className= "w-3/4 xl:w-3/6 flex relative gap-10 p-4 overflow-x-auto scroll-smooth scrollbar-thin scrollbar-thumb-secondary scrollbar-track-primary ">
         {
           skinsData?.map((skin) => {
             const urlImage= `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_${skin.num}.jpg`;
             return (
-              <button onClick={()=>{handleSkin(skin.num, skin.name)}} key={skin.name} className="max-w-[300px] min-w-[150px] max-h-48 rounded-sm focus:border-secondary">
+              <button onClick={()=>{handleSkin(skin.num, skin.name)}} key={skin.name} className={`max-w-[300px] min-w-[150px] max-h-48 rounded-sm border-2 ${skinSelected.name === skin.name? 'border-secondary': 'border-transparent'}  scale-110 hover:scale-100 transition-all duration-500`}>
                 <img  className=" object-contain rounded-sm" src={urlImage} />
               </button>
             )
